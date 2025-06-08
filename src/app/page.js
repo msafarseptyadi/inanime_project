468220d1467e535118c32254e403fcb14af33441
Muhammad Safar Septyadi <safar.septyadi@gmail.com>
2025-06-08 18:38:57 +0700

    feat: integrate Anilist API to display trending anime
    - Implemented API call to retrieve trending anime data
    - bug fixing genre filter

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,11 @@ import { useState, useRef, useCallback, useEffect } from "react";
 
 export default function Home() {
     const [animeList, setAnimeList] = useState([]);
+    const [animeTrendings, setAnimeTrendings] = useState([]);
     const [genres, setGenres] = useState([]);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(true);
+     const [loadingFirst, setLoadingFirst] = useState(true);
     const [hasMore, setHasMore] = useState(true);
     const [selectedGenre, setSelectedGenre] = useState("");
 
@@ -45,7 +47,8 @@ export default function Home() {
             }
         }
         }`;
-
+    
+    console.log("selectedGenre",selectedGenre);
     const query = selectedGenre ? queryWithGenre : queryWithoutGenre;
     const variables = { page: currentPage, perPage: 9 };
     if (selectedGenre && selectedGenre !== "0") {
@@ -65,16 +68,44 @@ export default function Home() {
     }, [selectedGenre]);
 
     useEffect(() => {
-        const fetchGenres = async () => {
-            const query = `query { genres: GenreCollection }`;
-            try {
-                const data = await getAnimeResponse(query);
-                setGenres(data.genres);
-            } catch (error) {
-                console.error("Error fetching genres:", error);
+        const fetchAnimeTrendings = async () => {
+        setLoadingFirst(true);
+        const query = `
+            query ($page: Int, $perPage: Int) {
+            Page(page: $page, perPage: $perPage) {
+                media(type: ANIME, sort: TRENDING_DESC) {
+                id
+                title { romaji english }
+                coverImage { large color }
+                description
+                bannerImage
+                genres
+                averageScore
+                }
             }
+            }`;
+
+        try {
+            const data = await getAnimeResponse(query, { page: 1, perPage: 3 });
+            setAnimeTrendings(data.Page.media);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoadingFirst(false);
+        }
         };
 
+        const fetchGenres = async () => {
+        const query = `query { genres: GenreCollection }`;
+        try {
+            const data = await getAnimeResponse(query);
+            setGenres(data.genres);
+        } catch (error) {
+            console.error("Error fetching genres:", error);
+        }
+        };
+
+        fetchAnimeTrendings();
         fetchGenres();
     }, []);
 
@@ -113,69 +144,12 @@ export default function Home() {
         fetchAnime(1, true);
     }, [selectedGenre, fetchAnime]);
 
-    const animeTrendings = [
-        {
-          "id": 156092,
-          "title": {
-            "english": "To Be Hero X",
-            "romaji": "Tu Bian Yingxiong X"
-          },
-          "description": "This is a world where heroes are created by people's trust, and the hero who gains the most trust is known as X. In this world, people's trust can be quantified through data, and these values are reflected on everyone's wrist. As long as one gains enough trust points, an ordinary person can possess superpowers and become a superhero who saves the world. However, the constantly changing trust values make the path of a hero full of uncertainties...\n<br><br>\n(Source: Official To Be Hero X home page)",
-          "genres": [
-            "Action",
-            "Drama",
-            "Mystery",
-            "Supernatural"
-          ],
-          "averageScore": 83,
-          "coverImage": {
-            "large": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx156092-yHqgQZOF2mbg.jpg"
-          }
-        },
-        {
-          "id": 180367,
-          "title": {
-            "english": "WITCH WATCH",
-            "romaji": "Witch Watch"
-          },
-          "description": "Morihito Otogi, a high school student who comes from a lineage of ogres, enjoys a peaceful, ordinary life until his childhood friend, Nico, moves in with him. Nico is a witch-in-training, and chooses Morihito to be her familiar. While Nico is thrilled to reunite with her old friend and crush, Morihito is tasked with the perilous duty to protect her from a foretold calamity. Between the unpredictable chaos caused by Nico’s magic, and the awkwardness of sharing a home, their lives become a whirlwind of supernatural hijinks and threats.\n<br><br>\n(Source: Crunchyroll) <br><br>\n\n<i>Notes: <br>\n- Worldwide premiere of Episodes 1-3 titled as <b>WITCH WATCH: WATCH PARTY</b> before the Japanese premiere was pre-screened in advance in theaters on March 16, 2025 in North America by GKIDS Films and March 22 and 23 in Europe by Animation Digital Network. <br>\n- Episodes 1-2 + a selection of stories (Cat Scout, Kanshi's Part Time Job Diaries ~ The Side Job ~, and Kan & Nico's Channel) was pre-screened in advance in theaters on March 30, 2025 in Japan. The regular TV broadcast began April 6, 2025.</i>",
-          "genres": [
-            "Comedy",
-            "Drama",
-            "Fantasy",
-            "Romance",
-            "Slice of Life",
-            "Supernatural"
-          ],
-          "averageScore": 73,
-          "coverImage": {
-            "large": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx180367-GlRuB2lG7Kaa.jpg"
-          }
-        },
-        {
-          "id": 176301,
-          "title": {
-            "english": "The Apothecary Diaries Season 2",
-            "romaji": "Kusuriya no Hitorigoto 2nd Season"
-          },
-          "description": "The second season of <i>Kusuriya no Hitorigoto</i>.<br><br>\n\nMaomao and Jinshi face palace intrigue as a pregnant concubine's safety and a looming conspiracy collide.<br><br>\n\n(Source: Crunchyroll News)",
-          "genres": [
-            "Drama",
-            "Mystery"
-          ],
-          "averageScore": 87,
-          "coverImage": {
-            "large": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx176301-TIGmldLffQGX.jpg"
-          }
-        },
-    ];
-
     return (
         <section>
             <div className="absolute mx-auto w-full">
             <div className="container mx-auto -mt-35 p-4">
                 <Header title={'Trending Now'} color={'text-white'} filter={false}/>
-                <AnimeList data={animeTrendings} />
+                <AnimeList data={animeTrendings} loading={loadingFirst} />
             </div>
             <div className="container mx-auto p-4 mb-100">
                 <Header title={'Anime'} color={'text-black'} filter={true} genres={genres} onGenreChange={handleGenreChange}/>
--- src/components/AnimeList/Header.js
+++ src/components/AnimeList/Header.js
@@ -5,7 +5,7 @@ export default function Header({title, color, filter, genres, onGenreChange}) {
         {filter &&
             <form>
                 <select onChange={(e) => onGenreChange(e.target.value)} id="genres" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option defaultValue={""}>Choose a Genre</option>
+                    <option value="">Choose a Genre</option>
                     {
                         genres.map((genre,i) => (
                             <option value={genre} key={i}>{genre}</option>
